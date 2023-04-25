07b93e9ece55213805dc3934807041aad0cbefe8
Valérian M <valerian.mermoz@gmail.com>
2023-04-25 18:27:27 +0200

    commit
    commit

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,32 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+class Carousel {
+	constructor (element,options ={}) {
+		this.element=element
+		this.options=Object.assign({},{
+			slidesToScroll:1,
+			slidesVisible:1
+		}, options)
+		this.children = [].slice.call(element.children)
+		let root = document.createElement('banner')
+		root.setAttribute('class', 'banner')
+		this.element.appendChild(root)
+	}
+
+	createDivWithClass (className) {
+		let div = document.createElement('div')
+		div.setAttribute('class', className)
+		return div
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+	new Carousel(document.querySelector('#caroussel1'), {
+	slidesToScroll:1,
+	slidesVisible:1
+	})
+})
+
