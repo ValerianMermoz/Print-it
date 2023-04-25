be02160169f4e237ced34c5df181a955bbd9b222
Valérian M <valerian.mermoz@gmail.com>
2023-04-25 23:08:52 +0200

    commit

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,66 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
-class Carousel {
-	constructor (element,options ={}) {
-		this.element=element
-		this.options=Object.assign({},{
-			slidesToScroll:1,
-			slidesVisible:1
-		}, options)
-		this.children = [].slice.call(element.children)
-		let root = document.createElement('banner')
-		root.setAttribute('class', 'banner')
-		this.element.appendChild(root)
-	}
+let position = 0; 
 
-	createDivWithClass (className) {
-		let div = document.createElement('div')
-		div.setAttribute('class', className)
-		return div
-	}
+createCaroussel(position);
+createDots();
+updateDot();
+function createDots(){
+	 const dots = document.querySelector(".dots");	
+	for (let index = 0; index < slides.length; index++) {
+		// Pour chaque element dans la boucle je vais créer un dot
+		const dot= document.createElement("div");
+		dot.setAttribute("class", "dot");
+		dots.appendChild(dot);
+	} 
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+function updateDot(){
+	const listPoints = document.querySelectorAll(".dot");	
+	
+   for (let index = 0; index < listPoints.length; index++) {
+	if(index == position){
+		const dot = listPoints[index];
+		dot.setAttribute("class", "dot dot_selected")
+	   console.log(listPoints[index]);	
+	}   //dot.setAttribute("class", "dot dot_selected");
 
-	new Carousel(document.querySelector('#caroussel1'), {
-	slidesToScroll:1,
-	slidesVisible:1
-	})
-})
+   } 
+}
 
+function createCaroussel(position){
+	const banner = document.querySelector("#banner");
+	
+		const element = slides[position];
+		//console.log(element);
+		const img = document.createElement("img");
+		img.setAttribute("class" , "banner-img");
+		img.setAttribute("src" , "./assets/images/slideshow/"+ element.image);
+		img.setAttribute("alt" , "Banner Print-it");
+		const p = document.createElement("p");
+		p.innerHTML= element.tagLine;
+		
+		banner.appendChild(img);
+		banner.append(p);
+			
+	
+
+}
